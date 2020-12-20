43446cb06ad450c7b51fba05ad6f50c68ae49682
SANDESH <sandeshfromindia@gmail.com>
2020-12-20 19:06:19 +0530

    adding other project files

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Blog Test!
+      </h1>
+      Write private blogs
+    </div>
+  );
+};
+
+export default Landing;
